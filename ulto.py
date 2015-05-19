b36475272eb4e5fa739c9dd2c300bd75a7557b37
PakornT <pakorn.ta@gmail.com>
2015-05-19 17:29:22 +0700

    Add read from config file

--- ulto.py
+++ ulto.py
@@ -4,21 +4,24 @@ import urllib.request as Request
 import subprocess
 import re
 import sys
-#print()
+
 if __name__ == '__main__':
+
 	if(sys.argv.__len__()<2):
 		print('Please speficied the URL.')
 		exit()
+	else:
+		URL = sys.argv[1]
 	
-	URL = sys.argv[1]
-
 	with open('conf','r') as f:
-		USERandPASS = f.read()
-	
+		HOST = str(sys.argv[0].split('.')[0])
+		while(f.readline().rstrip() != HOST):
+			True
+		USERandPASS = f.read().rstrip()
+		
 	with Request.urlopen(URL) as f:
 		pattern = '(?<=<title>).*(?= \(.*\) - uploaded.net</title>)'
 		fileTitle = re.findall(pattern, str(f.read(500)))[0]
 
 	subprocess.call('exec echo '+fileTitle, shell=True)
-	subprocess.call('exec echo '+USERandPASS, shell=True)
 #	subprocess.call('exec plowdown --auth='+USERandPASS+' '+URL+' > /dev/null &', shell=True)
