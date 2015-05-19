5e8b9a29d9e85d5f24357134228e068122f7fbe6
PakornT <pakorn.ta@gmail.com>
2015-05-19 16:46:27 +0700

    Add more commands

--- ulto.py
+++ ulto.py
@@ -1,9 +1,24 @@
 #!/usr/local/bin/python3
 
 import urllib.request as Request
+import subprocess
 import re
+import sys
 #print()
-with Request.urlopen('http://uploaded.net/file/u5bq81z6') as f:
-	pattern = '(?<=<title>).*(?= \(.*\) - uploaded.net</title>)'
-	fileTitle = re.findall(pattern, f.read(300))
-#	print(fileTitle)
+if __name__ == '__main__':
+	if(sys.argv.__len__()<2):
+		print('Please speficied the URL.')
+		exit()
+	
+	URL = sys.argv[1]
+
+	with open('conf','r') as f:
+		USERandPASS = f.read()
+	
+	with Request.urlopen(URL) as f:
+		pattern = '(?<=<title>).*(?= \(.*\) - uploaded.net</title>)'
+		fileTitle = re.findall(pattern, str(f.read(500)))[0]
+
+	subprocess.call('exec echo '+fileTitle, shell=True)
+	subprocess.call('exec echo '+USERandPASS, shell=True)
+#	subprocess.call('exec plowdown --auth='+USERandPASS+' '+URL+' > /dev/null &', shell=True)
